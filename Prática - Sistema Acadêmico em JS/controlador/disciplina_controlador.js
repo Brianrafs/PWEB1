6ff45e877866e42f88ab42941096ca2881009955
Brianrafs <106037880+Brianrafs@users.noreply.github.com>
2023-09-05 11:06:02 -0300

    Add files via upload

---
+++ Prática - Sistema Acadêmico em JS/controlador/disciplina_controlador.js
@@ -0,0 +1,51 @@
+class DisciplinaControlador {
+  
+  constructor() {
+    this.servico = new DisciplinaService()
+  }
+
+  inserir() {
+    const codigoElemento = document.querySelector('#codigo')
+    const nomeElemento = document.querySelector('#nome')
+    const disciplinaInserida = this.servico.inserir(
+      Number(codigoElemento.value),
+      nomeElemento.value
+    )
+    const listaDisciplinasElemento = document.querySelector('#listaDisciplinas')
+    if (disciplinaInserida) {
+      this.inserirDisciplinaNoHtml(disciplinaInserida, listaDisciplinasElemento)
+    }
+  }
+  remover(codigo) {
+    codigo = codigo.split('-')[1]
+    const disciplinaRemovida = this.servico.remover(Number(codigo))
+    if (disciplinaRemovida) {
+      this.removerDisciplinaDoHtml(codigo)
+    }
+  }
+
+  inserirDisciplinaNoHtml(disciplina, elementoDestino) {
+    const elementoDisciplina = document.createElement('li')
+    const codigo = disciplina.codigo
+    elementoDisciplina.setAttribute('id', `disciplina-${codigo}`)
+
+    const disciplinaText = document.createElement('p')
+    disciplinaText.textContent = `${disciplina}`
+    elementoDisciplina.appendChild(disciplinaText)
+
+    const disciplinaCloseButton = document.createElement('button')
+    disciplinaCloseButton.textContent = 'X'
+    disciplinaCloseButton.setAttribute('id', `remover-${codigo}`)
+    disciplinaCloseButton.addEventListener('click', click => {
+      const codigo = click.target.id
+      this.remover(codigo)
+    })
+    elementoDisciplina.appendChild(disciplinaCloseButton)
+    elementoDestino.appendChild(elementoDisciplina)
+  }
+
+  removerDisciplinaDoHtml(codigo) {
+    const disciplinaARemover = document.querySelector(`#disciplina-${codigo}`)
+    disciplinaARemover.remove()
+  }
+}
---
+++ Prática - Sistema Acadêmico em JS/servico/disciplina_servico.js
@@ -0,0 +1,44 @@
+class DisciplinaService {
+
+  constructor() {
+    this.repositorio = new DisciplinaRepositorio()
+  }
+
+  inserir(codigo, nome) {
+    const disciplinaPesquisada = this.pesquisarPorCodigo(codigo)
+    if (disciplinaPesquisada.length > 0) {
+      throw new Error('Disciplina já cadastrado!')
+    }
+    const DisciplinaNova = new Disciplina(codigo, nome)
+    this.repositorio.inserir(DisciplinaNova)
+    return DisciplinaNova
+  }
+
+  pesquisarPorCodigo(codigo) {
+    return this.repositorio
+        .listar()
+        .filter(disciplina => disciplina.codigo === codigo)
+  }
+
+  remover(codigo) {
+    const disciplinaPesquisada = this.pesquisarPorCodigo(codigo)
+    this.repositorio.remover(codigo)
+    return true
+  }
+
+  inserirAlunoNaDisciplina(aluno, codigo) {
+    const disciplina = this.pesquisarPorCodigo(codigo)
+    if (disciplina.length === 0) {
+      throw new Error('Disciplina não encontrada')
+    }
+    disciplina[0].alunosMatriculados.push(aluno)
+  }
+
+  atualizar(codigo, novoNome) {
+    const disciplina = this.pesquisarPorCodigo(codigo)
+    if (disciplina.length === 0) {
+      throw new Error('Disciplina não encontrada')
+    }
+    disciplina[0].nome = novoNome
+  }
+}
